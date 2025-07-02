a2e1889e04aca7f8d0bae3bc2e358ad861961fbc
divy-mohan <vedmatawebdesigning@gmail.com>
2025-07-02 14:57:04 +0530

    modified settings

--- pranteey_yuva/settings.py
+++ pranteey_yuva/settings.py
@@ -79,9 +79,15 @@ WSGI_APPLICATION = 'pranteey_yuva.wsgi.application'
 # Using DATABASE_URL for database configuration
 import dj_database_url
 
+import os
+
 DATABASES = {
-    'default': dj_database_url.parse(env('DATABASE_URL'))
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
 }
+
 
 # Password validation
 AUTH_PASSWORD_VALIDATORS = [
